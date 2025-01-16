2de70fa62a4b9170a73d98722e787f3efba5d194
MukitHossen7 <hossenmukit7@gmail.com>
2025-01-16 18:50:30 +0600

    Add react-rating-stars-component for enhanced review ratings and update MealDetails component for improved review submission and display

--- src/pages/MealDetails/MealDetails.jsx
+++ src/pages/MealDetails/MealDetails.jsx
@@ -6,6 +6,7 @@ import useAxiosPublic from "../../hooks/useAxiosPublic";
 import { AuthContext } from "../../providers/AuthProvider";
 import toast from "react-hot-toast";
 import useAxiosInstance from "../../hooks/useAxiosInstance";
+import ReactStars from "react-rating-stars-component";
 const MealDetails = () => {
   const { id } = useParams();
   const { user } = useContext(AuthContext);
@@ -60,6 +61,7 @@ const MealDetails = () => {
       refetch();
     }
   };
+
   return (
     <div>
       <div className="px-4 py-8 md:px-16 lg:px-24">
@@ -123,60 +125,93 @@ const MealDetails = () => {
         </div>
 
         {/* Reviews Section */}
-        <div className="mt-8">
-          <h2 className="text-2xl font-bold text-gray-800 mb-4">
-            Reviews ({detailsData?.reviewsCount})
-          </h2>
+        <div className="mt-8 flex flex-col lg:flex-row gap-8">
+          <div className="w-full lg:w-[40%]">
+            <h2 className="text-2xl font-bold text-gray-800 mb-4">
+              Reviews ({detailsData?.reviewsCount})
+            </h2>
 
-          <div className="mb-6">
-            <form onSubmit={handleReview}>
-              <div className="grid lg:grid-cols-2 gap-3 lg:gap-10">
-                <textarea
-                  name="description"
-                  placeholder="Write your review here..."
-                  className="w-full border rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
-                  rows="2"
-                  required
-                ></textarea>
-
-                {/* Rating Dropdown */}
-                <div className="mt-4">
-                  <label
-                    htmlFor="rating"
-                    className="block mb-2 text-base font-medium text-gray-700"
-                  >
-                    Rate your experience
-                  </label>
-                  <select
-                    id="rating"
-                    name="rating"
+            <div className="mb-6">
+              <form
+                onSubmit={handleReview}
+                className="bg-white shadow-md rounded-md p-4"
+              >
+                <div className="">
+                  <textarea
+                    name="description"
+                    placeholder="Write your review here..."
+                    className="w-full border rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                    rows="2"
                     required
-                    className="w-full border rounded-md px-2 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
-                  >
-                    <option value="" disabled selected>
-                      Select a rating
-                    </option>
-                    <option value="1">1 - Poor</option>
-                    <option value="2">2 - Fair</option>
-                    <option value="3">3 - Good</option>
-                    <option value="4">4 - Very Good</option>
-                    <option value="5">5 - Excellent</option>
-                  </select>
-                </div>
-              </div>
+                  ></textarea>
 
-              <button className="mt-4 bg-blue-500 text-white px-4 py-2 rounded-md shadow hover:bg-blue-600 transition">
-                Submit Review
-              </button>
-            </form>
+                  {/* Rating Dropdown */}
+                  <div className="mt-4">
+                    <label
+                      htmlFor="rating"
+                      className="block mb-2 text-base font-medium text-gray-700"
+                    >
+                      Rate your experience
+                    </label>
+                    <select
+                      id="rating"
+                      name="rating"
+                      required
+                      className="w-full border rounded-md px-2 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                    >
+                      <option value="" disabled selected>
+                        Select a rating
+                      </option>
+                      <option value="1">1 - Poor</option>
+                      <option value="2">2 - Fair</option>
+                      <option value="3">3 - Good</option>
+                      <option value="4">4 - Very Good</option>
+                      <option value="5">5 - Excellent</option>
+                    </select>
+                  </div>
+                </div>
+
+                <button className="mt-4 bg-blue-500 text-white px-4 py-2 rounded-md shadow hover:bg-blue-600 transition">
+                  Submit Review
+                </button>
+              </form>
+            </div>
           </div>
 
-          <div>
+          <div className="w-full lg:w-[60%] lg:mt-12">
             {reviews.length > 0 ? (
               reviews.map((review) => (
-                <div key={review._id} className=" py-2">
-                  <div className="bg-white shadow-md rounded-md px-4 py-4">
-                    <p>{review?.description}</p>
+                <div key={review._id} className="py-2">
+                  <div className="bg-white shadow-md rounded-md px-4 py-4 border border-gray-100 flex items-center gap-4">
+                    <div>
+                      <img
+                        src={review?.foodImg}
+                        className="w-14 h-14 object-cover rounded-md"
+                      ></img>
+                    </div>
+                    <div className="">
+                      <p className="text-lg font-semibold -mb-1">
+                        {review?.description}
+                      </p>
+                      <div>
+                        <ReactStars
+                          count={5}
+                          value={review?.rating}
+                          size={24}
+                          isHalf={true}
+                          emptyIcon={<i className="far fa-star"></i>}
+                          halfIcon={<i className="fa fa-star-half-alt"></i>}
+                          fullIcon={<i className="fa fa-star"></i>}
+                          activeColor={
+                            review?.rating >= 4
+                              ? "#4caf50"
+                              : review?.rating >= 3
+                              ? "#ffc107"
+                              : "#f44336"
+                          }
+                        />
+                      </div>
+                    </div>
                   </div>
                 </div>
               ))
