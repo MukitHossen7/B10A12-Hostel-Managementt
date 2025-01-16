ca8054769809a88cfb716902d621662d4845f839
MukitHossen7 <hossenmukit7@gmail.com>
2025-01-16 16:59:40 +0600

    Refactor MealDetails component to integrate dynamic review fetching, enhance like functionality with user authentication, and improve review submission process with form handling

--- src/pages/MealDetails/MealDetails.jsx
+++ src/pages/MealDetails/MealDetails.jsx
@@ -1,35 +1,65 @@
 import { useQuery } from "@tanstack/react-query";
-import { useState } from "react";
+import { useContext } from "react";
 import { FaThumbsUp } from "react-icons/fa";
 import { useParams } from "react-router-dom";
 import useAxiosPublic from "../../hooks/useAxiosPublic";
+import { AuthContext } from "../../providers/AuthProvider";
+import toast from "react-hot-toast";
+import useAxiosInstance from "../../hooks/useAxiosInstance";
 const MealDetails = () => {
   const { id } = useParams();
-  const [likeCount, setLikeCount] = useState(0);
-  const [reviews, setReviews] = useState([]);
+  const { user } = useContext(AuthContext);
   const axiosPublic = useAxiosPublic();
-  const { data: detailsData = {} } = useQuery({
+  const axiosInstance = useAxiosInstance();
+  const { data: detailsData = {}, refetch } = useQuery({
     queryKey: ["mealDetails", id],
     queryFn: async () => {
       const { data } = await axiosPublic.get(`/meal-details/${id}`);
       return data;
     },
   });
-  const handleLike = () => {
-    setLikeCount(likeCount + 1);
-    // Update like count on server
-  };
-
+  const { data: reviews = [] } = useQuery({
+    queryKey: ["reviews", id],
+    queryFn: async () => {
+      const { data } = await axiosPublic.get(`/reviews/${id}`);
+      return data;
+    },
+  });
+  console.log(reviews);
   const handleMealRequest = () => {
-    alert("Meal requested!");
-    // Post request to save meal and user info with pending status
+    console.log("request is working");
   };
 
-  const addReview = (review) => {
-    setReviews([...reviews, review]);
-    // Post review to server
+  const handleLike = async (likeId) => {
+    if (!user) {
+      toast.error("Please Login!");
+      return;
+    }
+    await axiosInstance.patch(`/update-like/${likeId}`);
+    toast.success("Like added successfully!");
+    refetch();
   };
-
+  const handleReview = async (e) => {
+    e.preventDefault();
+    const description = e.target.description.value;
+    const rating = parseInt(e.target.rating.value);
+    const reviewData = {
+      description,
+      rating,
+      foodId: detailsData?._id,
+      foodImg: detailsData?.image,
+    };
+    console.log(reviewData);
+    try {
+      await axiosPublic.post(`/reviews`, reviewData);
+      await axiosPublic.patch(`/update-reviews/${detailsData?._id}`);
+      e.target.reset();
+    } catch (error) {
+      console.log(error);
+    } finally {
+      refetch();
+    }
+  };
   return (
     <div>
       <div className="px-4 py-8 md:px-16 lg:px-24">
@@ -38,7 +68,7 @@ const MealDetails = () => {
           <div className="w-full lg:w-1/2">
             <img
               src={detailsData?.image}
-              className="rounded-lg shadow-md w-full h-96 lg:h-[500px] object-cover"
+              className="rounded-lg shadow-md w-full h-96 lg:h-[450px] object-cover"
             />
           </div>
 
@@ -77,10 +107,10 @@ const MealDetails = () => {
 
             <div className="flex items-center gap-4">
               <button
-                onClick={handleLike}
-                className="flex items-center gap-2 bg-blue-500 text-white px-4 py-2 rounded-md shadow hover:bg-blue-600 transition"
+                onClick={() => handleLike(detailsData._id)}
+                className={`flex items-center gap-2 bg-blue-500 text-white px-4 py-2 rounded-md shadow hover:bg-blue-600 transition `}
               >
-                <FaThumbsUp /> Like ({likeCount})
+                <FaThumbsUp /> Like({detailsData?.likes})
               </button>
               <button
                 onClick={handleMealRequest}
@@ -95,27 +125,59 @@ const MealDetails = () => {
         {/* Reviews Section */}
         <div className="mt-8">
           <h2 className="text-2xl font-bold text-gray-800 mb-4">
-            Reviews ({reviews.length})
+            Reviews ({detailsData?.reviewsCount})
           </h2>
 
           <div className="mb-6">
-            <textarea
-              placeholder="Write your review here..."
-              className="w-full border rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
-            ></textarea>
-            <button
-              onClick={() => addReview("This is a dummy review.")}
-              className="mt-2 bg-blue-500 text-white px-4 py-2 rounded-md shadow hover:bg-blue-600 transition"
-            >
-              Submit Review
-            </button>
+            <form onSubmit={handleReview}>
+              <div className="grid lg:grid-cols-2 gap-3 lg:gap-10">
+                <textarea
+                  name="description"
+                  placeholder="Write your review here..."
+                  className="w-full border rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                  rows="2"
+                  required
+                ></textarea>
+
+                {/* Rating Dropdown */}
+                <div className="mt-4">
+                  <label
+                    htmlFor="rating"
+                    className="block mb-2 text-base font-medium text-gray-700"
+                  >
+                    Rate your experience
+                  </label>
+                  <select
+                    id="rating"
+                    name="rating"
+                    required
+                    className="w-full border rounded-md px-2 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                  >
+                    <option value="" disabled selected>
+                      Select a rating
+                    </option>
+                    <option value="1">1 - Poor</option>
+                    <option value="2">2 - Fair</option>
+                    <option value="3">3 - Good</option>
+                    <option value="4">4 - Very Good</option>
+                    <option value="5">5 - Excellent</option>
+                  </select>
+                </div>
+              </div>
+
+              <button className="mt-4 bg-blue-500 text-white px-4 py-2 rounded-md shadow hover:bg-blue-600 transition">
+                Submit Review
+              </button>
+            </form>
           </div>
 
           <div>
             {reviews.length > 0 ? (
-              reviews.map((review, index) => (
-                <div key={index} className="border-b py-2">
-                  <p>{review}</p>
+              reviews.map((review) => (
+                <div key={review._id} className=" py-2">
+                  <div className="bg-white shadow-md rounded-md px-4 py-4">
+                    <p>{review?.description}</p>
+                  </div>
                 </div>
               ))
             ) : (
